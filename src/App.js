3afa1fc91380a27bd9880488b30c3c9dd3a3b80f
Alvin Allen <alaj6625@gmail.com>
2019-04-07 13:05:38 -0500

    added 3 components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className='container'>
+          <h2>Mern Stack - Todo App</h2>
+        </div>
+        <Route path='/' exact component={TodosList} />
+        <Route path='/edit/:id' component={EditTodo} />
+        <Route path='/create' component={CreateTodo} />
+      </Router>
     );
   }
 }
---
+++ src/components/create-todo.component.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class CreateTodo extends Component {
+   render() {
+      return (
+         <div>
+            <p>Welcome to todos create component</p>
+         </div>
+      )
+   }
+}   
